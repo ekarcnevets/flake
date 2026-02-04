98be2f1f26d17d5c49143a4d7ee4a02d8278e5da
Steven Crake <ekarcnevets@gmail.com>
2026-02-04 13:30:18 +0000

    feat: Add git configuration
    - Configure user info and SSH signing with 1Password
    - Add fzf-powered git aliases (addm, cb, fixup, etc)
    - Configure work-specific includes for nscale repos
    - Set up Zed as merge/diff tool
    - Configure diff-so-fancy integration
    - Set rebase, fetch, push, and core preferences

--- dev.nix
+++ dev.nix
@@ -48,7 +48,104 @@
 
   programs.git = {
     enable = true;
-    # userName = "Your Name";
-    # userEmail = "your.email@example.com";
+
+    settings = {
+      user = {
+        name = "Steven Crake";
+        email = "ekarcnevets@gmail.com";
+        signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJOkkyB57orOxVPLOZ8YhMDTmvqj5b2fi0Cy5DRlwuEZ";
+      };
+
+      alias = {
+        addm = "!git ls-files --deleted --modified --other --exclude-standard | fzf -0 -m --preview 'git diff --color=always {-1}' | xargs -r git add";
+        addmp = "!git ls-files --deleted --modified --exclude-standard | fzf -0 -m --preview 'git diff --color=always {-1}' | xargs -r -o git add -p";
+        cb = "!git branch --all | grep -v '^[*+]' | awk '{print $1}' | fzf -0 --preview 'git show --color=always {-1}' | sed 's/remotes\\/origin\\///g' | xargs -r git checkout";
+        cs = "!git stash list | fzf -0 --preview 'git show --pretty=oneline --color=always --patch \"$(echo {} | cut -d: -f1)\"' | cut -d: -f1 | xargs -r git stash pop";
+        db = "!git branch | grep -v '^[*+]' | awk '{print $1}' | fzf -0 --multi --preview 'git show --color=always {-1}' | xargs -r git branch --delete";
+        Db = "!git branch | grep -v '^[*+]' | awk '{print $1}' | fzf -0 --multi --preview 'git show --color=always {-1}' | xargs -r git branch --delete --force";
+        ds = "!git stash list | fzf -0 --preview 'git show --pretty=oneline --color=always --patch \"$(echo {} | cut -d: -f1)\"' | cut -d: -f1 | xargs -r git stash drop";
+        edit = "!git ls-files --modified --other --exclude-standard | sort -u | fzf -0 --multi --preview 'git diff --color {}' | xargs -r $EDITOR -p";
+        fixup = "!git log --oneline --no-decorate --no-merges | fzf -0 --preview 'git show --color=always --format=oneline {1}' | awk '{print $1}' | xargs -r git commit --fixup";
+        resetm = "!git diff --name-only --cached | fzf -0 -m --preview 'git diff --color=always {-1}' | xargs -r git reset";
+      };
+
+      gpg = {
+        format = "ssh";
+        ssh.program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
+      };
+
+      commit = {
+        gpgsign = true;
+        verbose = true;
+      };
+
+      tag = {
+        sort = "version:refname";
+        gpgsign = true;
+      };
+
+      includeIf."gitdir:/Users/steven/src/github.com/nscaledev/".path = "/Users/steven/src/github.com/nscaledev/.gitconfig";
+      includeIf."gitdir:/Users/steven/src/github.com/stevencrake-nscale/".path = "/Users/steven/src/github.com/stevencrake-nscale/.gitconfig";
+
+      column.ui = "auto";
+      branch.sort = "-committerdate";
+      init.defaultBranch = "main";
+
+      diff = {
+        algorithm = "histogram";
+        colorMoved = "plain";
+        mnemonicPrefix = true;
+        renames = true;
+        tool = "zed";
+      };
+
+      difftool."zed".cmd = "zed --wait --diff \"$LOCAL\" \"$REMOTE\"";
+
+      merge.tool = "zed";
+
+      mergetool."zed" = {
+        cmd = "zed --wait --merge \"$LOCAL\" \"$REMOTE\" \"$BASE\" \"$MERGED\"";
+        trustExitCode = true;
+      };
+
+      push = {
+        default = "simple";
+        autoSetupRemote = true;
+        followTags = true;
+      };
+
+      fetch = {
+        prune = true;
+        prunetags = false;
+        all = true;
+      };
+
+      help.autocorrect = "prompt";
+
+      rerere = {
+        enabled = true;
+        autoupdate = true;
+      };
+
+      core = {
+        eol = "lf";
+        autocrlf = "input";
+        excludesfile = "~/.gitignore";
+        pager = "diff-so-fancy | less --tabs=4 -R";
+      };
+
+      rebase = {
+        autoSquash = true;
+        autoStash = true;
+        updateRefs = true;
+      };
+
+      pull.rebase = true;
+
+      "diff-so-fancy" = {
+        markEmptyLines = false;
+        stripLeadingSymbols = false;
+      };
+    };
   };
 }
--- home.nix
+++ home.nix
@@ -12,6 +12,51 @@
     _1password-gui
   ];
 
+  # Work-specific git config
+  home.file."src/github.com/nscaledev/.gitconfig".text = ''
+    [user]
+      name = "Steven Crake"
+      email = "steven.crake@nscale.com"
+      signingkey = ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILoxhIV4lNUvgM/1ORLBrc+tmr7esOJqRGZWUShgRMfj
+
+    [gpg]
+      format = ssh
+
+    [gpg "ssh"]
+      program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign"
+
+    [commit]
+      gpgsign = true
+
+    [tag]
+      gpgsign = true
+
+    [url "git@github.com-nscale:"]
+        insteadOf = git@github.com:
+  '';
+
+  home.file."src/github.com/stevencrake-nscale/.gitconfig".text = ''
+    [user]
+      name = "Steven Crake"
+      email = "steven.crake@nscale.com"
+      signingkey = ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILoxhIV4lNUvgM/1ORLBrc+tmr7esOJqRGZWUShgRMfj
+
+    [gpg]
+      format = ssh
+
+    [gpg "ssh"]
+      program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign"
+
+    [commit]
+      gpgsign = true
+
+    [tag]
+      gpgsign = true
+
+    [url "git@github.com-nscale:"]
+        insteadOf = git@github.com:
+  '';
+
   programs.zsh = {
     enable = true;
     oh-my-zsh = {
