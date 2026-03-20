0e7493d7e6f91f3ab04a8fac3f111ab14a8fe234
Steven Crake <ekarcnevets@gmail.com>
2026-03-20 18:05:59 +0000

    feat: Add azure-cli and azure-functions-core-tools to wagestation

--- hosts/wagestation/default.nix
+++ hosts/wagestation/default.nix
@@ -1,6 +1,13 @@
 { ... }: {
   networking.hostName = "wagestation";
 
+  homebrew.taps = [ "azure/functions" ];
+
+  homebrew.brews = [
+    "azure-cli"
+    "azure-functions-core-tools"
+  ];
+
   homebrew.casks = [
     "microsoft-teams"
     "ngrok"
